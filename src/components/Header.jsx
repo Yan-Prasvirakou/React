3d920347002c6ae9a0ea8339b56e0b28f6b728ca
nthngHL <soliankaa@gmail.com>
2021-02-07 20:11:00 +0200

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+	return (
+		<header className='header'>
+			<img src='https://ggscore.com/media/logo/t14977.png' width='50'/>
+		</header>
+	)
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Profile = () => {
+	return (
+		<div className='content'>
+			<div><img src='https://www.aeroflot.ru/media/aflfiles/by/msq/msq_2.jpg' height='100' /></div>
+			<div>Ava + discription</div>
+			<div>
+				my posts
+					<div>new post</div>
+			</div>
+			<div>posts
+					<div>post1</div>
+				<div>post2</div>
+			</div>
+		</div>
+	)
+}
+
+export default Profile;
