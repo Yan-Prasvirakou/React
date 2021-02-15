ac65f49d27c30804d3c90b10da184705b0b91001
nthngHL <soliankaa@gmail.com>
2021-02-15 21:31:45 +0200

    dialoga-and-messages-added-through-props

--- src/components/Dialogs/Messages/TanyaMessages.jsx
+++ src/components/Dialogs/Messages/TanyaMessages.jsx
@@ -6,6 +6,7 @@ const NastyaMessages = () => {
 	return (
 		<div className={classes.messages}>
 			<div className={classes.message}>Hi, I have waited for you so long</div>
+			<div className={classes.message}>I wanna see u</div>
 			<div className={classes.message}>I miss you</div>
 		</div>
 	)
