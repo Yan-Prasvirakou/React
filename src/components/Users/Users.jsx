64bef7c0490f1806b92746bf29e6dee919fa4298
nthngHL <soliankaa@gmail.com>
2021-03-14 22:08:36 +0200

    axios-query

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,16 +1,25 @@
+import axios from 'axios';
 import React from 'react';
 import classes from './Users.module.css';
-
+import MishaAva from './img/MishaAva.jpg';
 
 
 const Users = (props) => {
+	if (props.usersPage.users.length === 0) {
+		axios.get('https://social-network.samuraijs.com/api/1.0/users').then(res => {
+			props.setUsers(res.data.items)
+		})
+	}
+
 	return <div className={classes.usersWrap}>
-			<h2>Users-list</h2>
+		<h2>Users-list</h2>
+	
+		
 			{
 				props.usersPage.users.map(user => <div key={user.id} className={classes.userItem}>
 						<div className={classes.avaAndBtn}>
 							<div className={classes.ava}>
-								<img src={user.ava}/>
+							<img src={user.photos.small == null ? MishaAva : user.photos.small}/>
 							</div>
 						
 						{user.followed 
@@ -18,8 +27,8 @@ const Users = (props) => {
 						: <button onClick={()=> props.follow(user.id)} className={classes.btn}>follow</button>}
 						</div>
 						<div className={classes.userInfo}>
-							<h3>{user.fullName}</h3>
-						<div>{user.location.country}, {user.location.city}</div>
+							<h3>{user.name}</h3>
+						<div>user.location.country, user.location.city</div>
 							<div>{user.status}</div>
 						</div>
 					</div>
--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -8,49 +8,50 @@ let UNFOLLOW = 'UNFOLLOW';
 let SET_USERS = 'SET-USERS';
 
 let initialState = {
-	users: [
-		{ 
-			id: 1, 
-			followed: false, 
-			fullName: 'Pasha', 
-			status: 'i like react', 
-			location: { 
-				city: 'Minsk', country: 'Belarus' 
-			},
-			ava: PashaAva,
-		},
-		{ 
-			id: 2, 
-			followed: true,
-			fullName: 'Dasha',
-			status: 'i\'m from Rovno',
-			location: {
-				city: 'Rovno', country: 'Ukraine'
-			},
-			ava: DashaAva,
-		},
-		{ 
-			id: 3, 
-			followed: false, 
-			fullName: 'Misha', 
-			status: 'Hi everyone',
-			location: {
-				city: 'Vitebsk', country: 'Belarus'
-			},
-			ava: MishaAva,
-		},
-		{ 
-			id: 4, 
-			followed: true,
-			fullName: 'Inna',
-			status: 'Lego forever',
-			location: {
-				city: 'Odessa', country: 'Ukraine'
-			},
-			ava: InnaAva,
-		},
+	users: [],
+	// [
+	// 	{ 
+	// 		id: 1, 
+	// 		followed: false, 
+	// 		fullName: 'Pasha', 
+	// 		status: 'i like react', 
+	// 		location: { 
+	// 			city: 'Minsk', country: 'Belarus' 
+	// 		},
+	// 		ava: PashaAva,
+	// 	},
+	// 	{ 
+	// 		id: 2, 
+	// 		followed: true,
+	// 		fullName: 'Dasha',
+	// 		status: 'i\'m from Rovno',
+	// 		location: {
+	// 			city: 'Rovno', country: 'Ukraine'
+	// 		},
+	// 		ava: DashaAva,
+	// 	},
+	// 	{ 
+	// 		id: 3, 
+	// 		followed: false, 
+	// 		fullName: 'Misha', 
+	// 		status: 'Hi everyone',
+	// 		location: {
+	// 			city: 'Vitebsk', country: 'Belarus'
+	// 		},
+	// 		ava: MishaAva,
+	// 	},
+	// 	{ 
+	// 		id: 4, 
+	// 		followed: true,
+	// 		fullName: 'Inna',
+	// 		status: 'Lego forever',
+	// 		location: {
+	// 			city: 'Odessa', country: 'Ukraine'
+	// 		},
+	// 		ava: InnaAva,
+	// 	},
 
-	]
+	// ]
 };
 
 
